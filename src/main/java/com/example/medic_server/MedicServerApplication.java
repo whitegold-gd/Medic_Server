8543065aa112057f1ee5e3bc705a478495fecf6d
whitegold-gd <bonnie.terebko@gmail.com>
2021-11-13 11:08:53 +0300

    Initial commit

---
+++ src/main/java/com/example/medic_server/MedicServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.medic_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicServerApplication.class, args);
+	}
+
+}
